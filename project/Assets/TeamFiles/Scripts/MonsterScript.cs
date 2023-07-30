76bdeb62b519199bbcafdc7f70d9fe4cc35b438c
Martin <zyjinxx@hotmail.com>
2023-07-31 01:20:42 +0200

    mob spawner and mob movement take 1

--- project/Assets/TeamFiles/Scripts/MonsterScript.cs
+++ project/Assets/TeamFiles/Scripts/MonsterScript.cs
@@ -7,17 +7,33 @@ public class MonsterScript : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent agent;
+
+    public float moveInterval;
+    private float moveTimer = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        moveInterval = Random.Range(1f, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (moveTimer <= 0)
+        {
+            MonsterMove();
+            moveTimer = moveInterval;
+        }
+
+        moveTimer -= Time.deltaTime;
+    }
+
+    void MonsterMove()
+    {
+        agent.SetDestination(transform.position + new Vector3(Random.Range(-4f, 4f), 0, 0));
     }
 }
---
+++ project/Assets/TeamFiles/Scripts/MonsterSpawnerManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawnerManager : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    public Transform[] spawnPoints;
+
+    public float spawnInterval = 5f;
+    private float spawnTimer = 0f;
+    private int spawnCount = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer <= 0 && spawnCount > 0)
+        {
+            SpawnEnemy();
+            spawnTimer = spawnInterval;
+            spawnCount -= 1;
+        }
+
+        spawnTimer -= Time.deltaTime;
+    }
+
+    void SpawnEnemy()
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogWarning("No spawn points found. Please add child GameObjects to this spawner to act as spawn points.");
+            return;
+        }
+
+        Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(monsterPrefab, randomSpawnPoint.position + new Vector3(Random.Range(-4f, 4f), 0, 0), Quaternion.identity);
+    }
+}
