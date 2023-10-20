7fb24fd0cd0a3f607dcbaf398d98bd3983861df5
jgd <jgd@netcompany.com>
2023-10-20 17:44:46 +0200

    blacksmith canvas functionality is set up, needs tweaks and refactor

--- project/Assets/TeamFiles/Scripts/MenuScripts/UpgradeController.cs
+++ project/Assets/TeamFiles/Scripts/MenuScripts/UpgradeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
+using UnityEngine.UI;
 
 public class UpgradeController : MonoBehaviour
 {
@@ -38,6 +39,31 @@ public class UpgradeController : MonoBehaviour
     
     private List<Tuple<TextMeshProUGUI, string>> upgradeList;
 
+    public Color selectedColor;
+    public Color unselectedColor;
+    public Image swordIcon;
+    public Image helmIcon;
+    public Image shieldIcon;
+    public TextMeshProUGUI swordContext;
+    public TextMeshProUGUI helmContext;
+    public TextMeshProUGUI shieldContext;
+    public Image swordBuyButtonIcon;
+    public Image helmBuyButtonIcon;
+    public Image shieldBuyButtonIcon;
+    public TextMeshProUGUI swordBuytext;
+    public TextMeshProUGUI helmBuytext;
+    public TextMeshProUGUI shieldBuytext;
+    public Button swordButton;
+    public Button helmButton;
+    public Button shieldButton;
+    public Image swordButtonbg;
+    public Image helmButtonbg;
+    public Image shieldButtonbg;
+    public TextMeshProUGUI npcDmgText;
+    public TextMeshProUGUI npcAttspdText;
+    public TextMeshProUGUI npcHealthText;
+    public TextMeshProUGUI npcArmorText;
+    
     void Start()
     {
         // Set initial currency text
@@ -64,6 +90,15 @@ public class UpgradeController : MonoBehaviour
         {
             upgrade.Item1.text = PlayerPrefs.GetInt(upgrade.Item2).ToString();
         }
+
+        var upgradex = upgradeList[1];
+        swordBuytext.text = (PlayerPrefs.GetInt(upgradex.Item2) * priceMultiplier).ToString();
+
+        // refresh npc icons text
+        npcDmgText.text = PlayerPrefs.GetInt(upgradeList[0].Item2).ToString();
+        npcAttspdText.text = PlayerPrefs.GetInt(upgradeList[1].Item2).ToString();
+        npcHealthText.text = PlayerPrefs.GetInt(upgradeList[2].Item2).ToString();
+        npcArmorText.text = PlayerPrefs.GetInt(upgradeList[3].Item2).ToString();
     }
 
     public void Back()
@@ -88,7 +123,102 @@ public class UpgradeController : MonoBehaviour
         mainCurrencyText.text = PlayerPrefs.GetInt(stringManager.currency).ToString();
         
         // Upgrade
-        PlayerPrefs.SetInt(upgrade.Item2, PlayerPrefs.GetInt(upgrade.Item2) + 1);
+        var upgradeMultiplier = upgradeListIndex > 1 ? upgradeListIndex == 2 ? 5 : 2 : 1; 
+        PlayerPrefs.SetInt(upgrade.Item2, PlayerPrefs.GetInt(upgrade.Item2) + upgradeMultiplier);
         upgrade.Item1.text = PlayerPrefs.GetInt(upgrade.Item2).ToString();
+
+        var currencyTextObj = upgradeListIndex > 1 ? upgradeListIndex == 2 ? helmBuytext : shieldBuytext : swordBuytext; 
+        currencyTextObj.text = (PlayerPrefs.GetInt(upgrade.Item2) * priceMultiplier).ToString();
+
+        // refresh npc icons text
+        npcDmgText.text = PlayerPrefs.GetInt(upgradeList[0].Item2).ToString();
+        npcAttspdText.text = PlayerPrefs.GetInt(upgradeList[1].Item2).ToString();
+        npcHealthText.text = PlayerPrefs.GetInt(upgradeList[2].Item2).ToString();
+        npcArmorText.text = PlayerPrefs.GetInt(upgradeList[3].Item2).ToString();
+    }
+
+    public void SelectSwordUpgrade()
+    {
+        swordIcon.color = selectedColor;
+        helmIcon.color = unselectedColor;
+        shieldIcon.color = unselectedColor;
+
+        swordContext.enabled = true;
+        helmContext.enabled = false;
+        shieldContext.enabled = false;
+
+        swordButton.enabled = true;
+        helmButton.enabled = false;
+        shieldButton.enabled = false;
+        
+        swordButtonbg.enabled = true;
+        helmButtonbg.enabled = false;
+        shieldButtonbg.enabled = false;
+
+        swordBuyButtonIcon.enabled = true;
+        helmBuyButtonIcon.enabled = false;
+        shieldBuyButtonIcon.enabled = false;
+        swordBuytext.enabled = true;
+        helmBuytext.enabled = false;
+        shieldBuytext.enabled = false;
+
+        var upgrade = upgradeList[1];
+        swordBuytext.text = (PlayerPrefs.GetInt(upgrade.Item2) * priceMultiplier).ToString();
+    }
+    public void SelectHelmUpgrade()
+    {
+        swordIcon.color = unselectedColor;
+        helmIcon.color = selectedColor;
+        shieldIcon.color = unselectedColor;
+
+        swordContext.enabled = false;
+        helmContext.enabled = true;
+        shieldContext.enabled = false;
+
+        swordButton.enabled = false;
+        helmButton.enabled = true;
+        shieldButton.enabled = false;
+
+        swordButtonbg.enabled = false;
+        helmButtonbg.enabled = true;
+        shieldButtonbg.enabled = false;
+
+        swordBuyButtonIcon.enabled = false;
+        helmBuyButtonIcon.enabled = true;
+        shieldBuyButtonIcon.enabled = false;
+        swordBuytext.enabled = false;
+        helmBuytext.enabled = true;
+        shieldBuytext.enabled = false;
+
+        var upgrade = upgradeList[2];
+        helmBuytext.text = (PlayerPrefs.GetInt(upgrade.Item2) * priceMultiplier).ToString();
+    }
+    public void SelectShieldUpgrade()
+    {
+        swordIcon.color = unselectedColor;
+        helmIcon.color = unselectedColor;
+        shieldIcon.color = selectedColor;
+
+        swordContext.enabled = false;
+        helmContext.enabled = false;
+        shieldContext.enabled = true;
+
+        swordButton.enabled = false;
+        helmButton.enabled = false;
+        shieldButton.enabled = true;
+
+        swordButtonbg.enabled = false;
+        helmButtonbg.enabled = false;
+        shieldButtonbg.enabled = true;
+
+        swordBuyButtonIcon.enabled = false;
+        helmBuyButtonIcon.enabled = false;
+        shieldBuyButtonIcon.enabled = true;
+        swordBuytext.enabled = false;
+        helmBuytext.enabled = false;
+        shieldBuytext.enabled = true;
+
+        var upgrade = upgradeList[3];
+        shieldBuytext.text = (PlayerPrefs.GetInt(upgrade.Item2) * priceMultiplier).ToString();
     }
 }
