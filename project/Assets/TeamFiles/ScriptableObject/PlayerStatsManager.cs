9b0ef6b4ce64c1efc293caf2d608a8794ddbb2e0
jgd <jgd@netcompany.com>
2023-10-23 01:01:02 +0200

    small attack speed adjustment

--- project/Assets/TeamFiles/ScriptableObject/PlayerStatsManager.cs
+++ project/Assets/TeamFiles/ScriptableObject/PlayerStatsManager.cs
@@ -7,7 +7,7 @@ public class PlayerStatsManager : ScriptableObject
     public int damage = 1;
     public int health = 10;
     public int armor = 5;
-    public float attackSpeed = 10.0f;
+    public float attackSpeed = 5.0f;
     public int kills = 0;
     public float volume = 1;
     public int loot = 0;
@@ -21,7 +21,7 @@ public class PlayerStatsManager : ScriptableObject
         damage = 1;
         armor = 0;
         health = 10;
-        attackSpeed = 10.0f;
+        attackSpeed = 5.0f;
         kills = 0;
         loot = 0;
     }
