c3aa5934066249e77799bb0a02bf43d3ec6be5d6
jgd <jgd@netcompany.com>
2023-10-26 21:24:19 +0200

    redundant mob health check

--- project/Assets/TeamFiles/Scripts/GameScripts/MonsterScript.cs
+++ project/Assets/TeamFiles/Scripts/GameScripts/MonsterScript.cs
@@ -178,7 +178,6 @@ public class MonsterScript : MonoBehaviour
             if(playerStatsManager.perk2 == 1 && playerStatsManager.armor > 0)
             {
                 health -= damage;
-                if (health > 0) return;
                 MonsterDeath();
             }
         }
